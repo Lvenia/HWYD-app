de0c6648b2a754edccd82ec8a2b6c74bdab9f368
Iryna Kresinska <kresi.ira@gmail.com>
2020-10-26 13:01:55 +0100

    Build ActivitySection

--- src/components/Quiz/ActivitiesCategory.js
+++ src/components/Quiz/ActivitiesCategory.js
@@ -1,26 +1,53 @@
 import React from 'react';
-
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 import QuizCard from './QuizCard';
 import questions from './questions';
-
 import { CATEGORY_ACTIVITY } from '../../constants'
+import DropdownComponent from '../DropdownComponent';
 
 class ActivitiesCategory extends React.Component {
 
+  state = {}
+
+  handleInputChange = (activity, inputValue) => {
+    this.setState({
+      [activity]: {
+        ...this.state[activity],
+        activityTime: inputValue
+      }
+    })
+  }
+
+  handleSelectChange = (activity, selectedOption) => {
+    this.setState({
+      [activity]: {
+        ...this.state[activity],
+        energyImpact: selectedOption
+      }
+    })
+  }
+
+
   renderActivityCards() {
-    const activities = questions.filter(q => q.questionCategory === CATEGORY_ACTIVITY)
+
+    const activities = questions.filter(q => q.questionCategory === CATEGORY_ACTIVITY);
+
     return activities.map(a => {
       return (
         <QuizCard
           key={a.name}
-        />
-      )
-    })
-    //each q has question to be in UI and name to be in a state
+          activity={a.question}
+          onInputChange={this.handleInputChange}
+        >
+          <DropdownComponent
+            options={['uplifting', 'neutral', 'discouraging']}
+            activity={a.question}
+            onImpactSelect={this.handleSelectChange}
+          />
+        </QuizCard>
+      );
+    });
   }
 
   render() {
@@ -29,7 +56,6 @@ class ActivitiesCategory extends React.Component {
         <div className="p-3 justify-content-md-left">
           <h4>How much time have you spent on...</h4>
         </div>
-
         <Row className="justify-content-md-center">
           {this.renderActivityCards()}
         </Row>
--- src/components/Quiz/QuizCard.js
+++ src/components/Quiz/QuizCard.js
@@ -1,43 +1,39 @@
 import React from 'react';
 
 import Card from 'react-bootstrap/Card';
-import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-const QuizCard = () => {
+const QuizCard = (props) => {
   return (
     <Card style={{ width: '14rem', margin: '1%' }}>
       <Card.Body>
+
         <Col xs={12}>
           <Row className="justify-content-md-center">
-            <Card.Title>ACTIVITY NAME</Card.Title>
+            <Card.Title>{props.activity}</Card.Title>
           </Row>
 
           <Row className="justify-content-md-center">
-            <Col xs={12}>
-
-              <Form.Control size="md" type="number" placeholder="HOURS" />
-
-              <Dropdown>
-                <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                  MOOD IMPACT
-               </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">uplifting</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">neutral</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">discouraging</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+            <Col xs={12} >
+              <Form.Control
+                style={{ border: 'solid 1px gray' }}
+                min="0"
+                max="24"
+                step="0.5"
+                size="md"
+                type="number"
+                placeholder="HOURS"
+                onChange={(event) => { props.onInputChange(props.activity, event.target.value) }}
+              />
+              {props.children}
             </Col>
           </Row>
-
         </Col>
-
       </Card.Body>
     </Card>
-  )
+  );
 };
 
-export default QuizCard
+export default QuizCard;

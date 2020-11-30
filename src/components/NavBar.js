f29a2393f000fb9fffd3110ea5455eabc71e28cd
Iryna Kresinska <kresi.ira@gmail.com>
2020-11-30 10:47:06 +0100

    refacture navBar active

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,44 +2,57 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { logOut } from '../actions';
+import Nav from 'react-bootstrap/Nav'
 
 
 class NavBar extends React.Component {
 
-  renderButtons() {
+  //   <Nav variant="tabs" defaultActiveKey="/home">
+  //   <Nav.Item>
+  //     <Nav.Link href="/home">Active</Nav.Link>
+  //   </Nav.Item>
+  //   <Nav.Item>
+  //     <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+  //   </Nav.Item>
+  //   <Nav.Item>
+  //     <Nav.Link eventKey="disabled" disabled>
+  //       Disabled
+  //     </Nav.Link>
+  //   </Nav.Item>
+  // </Nav>
 
-    const buttons = [
+  renderTabs() {
+
+    const navBarTabs = [
       {
         name: 'Start',
-        path: "/"
+        path: "/home"
       },
       {
         name: 'Quiz',
         path: "/quiz"
       },
       {
-        name: 'Day',
+        name: 'Day Review',
         path: "/day"
       },
       {
-        name: 'All data',
+        name: 'Overview',
         path: "/data"
-      },
-      {
-        name: 'Users',
-        path: "/users"
-      },
+      }
     ];
 
     if (this.props.auth.isAuthenticated) {
-      return buttons.map((button, index) => {
+
+      return navBarTabs.map((tab, index) => {
         return (
           <NavLink
             className="nav-link"
-            to={button.path}
+            // to={`exact ${tab.path}`}
+            to={tab.path}
             key={index}
           >
-            <li className="nav-item">{button.name}</li>
+            <li className="nav-item">{tab.name}</li>
           </NavLink>
         );
       });
@@ -47,9 +60,9 @@ class NavBar extends React.Component {
       return (
         <NavLink
           className="nav-link"
-          to={buttons[0].path}
+          to={navBarTabs[0].path}
         >
-          <li className="nav-item">{buttons[0].name}</li>
+          <li className="nav-item">{navBarTabs[0].name}</li>
         </NavLink>
       )
     }
@@ -67,14 +80,9 @@ class NavBar extends React.Component {
       )
     } else {
       return (
-        <>
-          <a href="http://localhost:4000/auth/google">
-            Google
-          </a>
-          <a href="http://localhost:4000/auth/facebook">
-            Log in with Facebook
-          </a>
-        </>
+        <a href="http://localhost:4000/auth/google">
+          Log in with Google
+        </a>
       )
     }
   }
@@ -83,7 +91,7 @@ class NavBar extends React.Component {
     return (
       <div>
         <ul className="nav nav-tabs" >
-          {this.renderButtons()}
+          {this.renderTabs()}
           {this.renderAuthButtons()}
         </ul>
       </div >
@@ -98,3 +106,107 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, { logOut })(NavBar);
+
+
+
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { NavLink } from 'react-router-dom';
+// import { logOut } from '../actions';
+
+
+// class NavBar extends React.Component {
+
+//   renderButtons() {
+
+//     const buttons = [
+//       {
+//         name: 'Start',
+//         path: "/"
+//       },
+//       {
+//         name: 'Quiz',
+//         path: "/quiz"
+//       },
+//       {
+//         name: 'Day',
+//         path: "/day"
+//       },
+//       {
+//         name: 'All data',
+//         path: "/data"
+//       },
+//       {
+//         name: 'Users',
+//         path: "/users"
+//       },
+//     ];
+
+//     if (this.props.auth.isAuthenticated) {
+//       return buttons.map((button, index) => {
+//         return (
+//           <NavLink
+//             className="nav-link"
+//             to={button.path}
+//             key={index}
+//           >
+//             <li className="nav-item">{button.name}</li>
+//           </NavLink>
+//         );
+//       });
+//     } else {
+//       return (
+//         <NavLink
+//           className="nav-link"
+//           to={buttons[0].path}
+//         >
+//           <li className="nav-item">{buttons[0].name}</li>
+//         </NavLink>
+//       )
+//     }
+
+//   }
+
+//   renderAuthButtons() {
+//     if (this.props.auth.isAuthenticated) {
+//       return (
+//         <button
+//           onClick={this.props.logOut}
+//         >
+//           Log Out
+//         </button>
+//       )
+//     } else {
+//       return (
+//         <>
+//           <a href="http://localhost:4000/auth/google">
+//             Google
+//           </a>
+//           <a href="http://localhost:4000/auth/facebook">
+//             Log in with Facebook
+//           </a>
+//         </>
+//       )
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <ul className="nav nav-tabs" >
+//           {this.renderButtons()}
+//           {this.renderAuthButtons()}
+//         </ul>
+//       </div >
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     auth: state.auth
+//   }
+// }
+
+// export default connect(mapStateToProps, { logOut })(NavBar);
